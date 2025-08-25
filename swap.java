35f3112e9c8dfa077464ed998f151948e492d8d4
prajapati pranav <pranavprajapati9005@gmail.com>
2025-08-25 17:41:57 +0530

    Update swap.java

--- swap.java
+++ swap.java
@@ -2,7 +2,7 @@ public class swap {
     public static void main(String[] args) {
         int a=2,b=3;
        System.out.println("Before Swap: a = " + a + ", b = " + b);
-
+//swapping using Arithmatic operator
         a = a + b; 
         b = a - b; 
         a = a - b; 
